bb674a17b2c4966b79f831697866c2dc249c3339
zubair <mzubairnasim37@gmail.com>
2020-11-13 18:05:31 +0500

    make it better

--- src/componentsaction/Search.js
+++ src/componentsaction/Search.js
@@ -5,7 +5,7 @@ export default function Search() {
     const {state,dispatch} = useContext(Context)
     const [userInput, setUserInput] = useState("")
 
-    const api=`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${userInput}&apikey=6809450facf7b1b9e5ab776a6ac20587`
+    const api=`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?&page_size=5&q_track=${userInput}&apikey=6809450facf7b1b9e5ab776a6ac20587`
    
     const search=async()=>{
         const res=await fetch(api)
